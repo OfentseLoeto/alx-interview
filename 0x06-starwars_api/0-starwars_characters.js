d98c7213892a3d4bc3359dbc4ef8a6e4a5e8d38e
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-11-09 20:01:36 -0800

    Retrieve the movie data by making a request to the api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,33 @@
-#!/usr/bin/node
-import sys
-import request
+const axios = require('axios');
 
+// Check if the user provided a movie ID as a command-line argument
+if (process.argv.length < 3) {
+    console.log("Usage: node 0-starwars_characters.js <Movie ID>");
+    process.exit(1);
+}
 
-if len(sys.arv) < 2:
-    print("Usage: Python 0-starwars_characters.js <Movie ID")
-    sys.exit(1)
+// Define the Star Wars API base URL
+const baseUrl = "https://swapi.dev/api";
 
-# define the Star_war API base url
-base_url = "https://swapi.dev/api"
+// Get the movie ID from the command-line argument
+const movieId = process.argv[2];
 
-# Get the movie id fromthe command-line arguments
-movie_id = sys.argv[1]
+// Make a request to the /films endpoint to retrieve information about the movie
+const filmUrl = `${baseUrl}/films/${movieId}/`;
 
-# Make a request to the /films endpoint to retrieve information
-film_url = f"[base_url}/films/{movie_id}/"
-response = requests.get(film_url)
+axios.get(filmUrl)
+    .then(response => {
+        const filmData = response.data;
 
-# Check if request was successful
-if response.status_code == 200:
-    film_data = response.json()
-    
-    # Retrieve the list of character URLs from the movie data
-    characters_urls = 'characters'
+        // Retrieve the list of character URLs from the movie data
+        const characterUrls = filmData['characters'];
 
-    # Print the characters' names
-    for character_url in character_urls:
-        character_response = requests.get(character_url)
-        if character_response.status_code == 200:
-            character_data = character_response.json()
-            print(character_data['name'])
-        else:
-            print(f"Failed to fetch character data from {character_url}")
-else:
-    print(f"Failed to fetch movie data for Movie ID {movie_id}")
+        // Print the characters' names
+        Promise.all(characterUrls.map(characterUrl =>
+            axios.get(characterUrl)
+                .then(characterResponse => console.log(characterResponse.data['name']))
+                .catch(error => console.error(`Failed to fetch character data from ${characterUrl}`, error))
+        ))
+            .catch(error => console.error(error));
+    })
+    .catch(error => console.error(`Failed to fetch movie data for Movie ID ${movieId}`, error));
