3a5836b24a3e1397a5e44cead446f7c01b280bcb
ofentse loeto <ohentseloeto610@gmail.com>
2023-11-23 18:17:43 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,21 @@ Make change depending on the given value
 
 
 def makeChange(coins, total):
+    """
+    Calculate the fewest number of coins needed to meet a given total.
+
+    Args:
+      - coins (list): A list of integers representing the values of
+        available coins.
+      - total (int): The target total amount to be achieved using the
+        available coins.
+
+    Return:
+      - int: The fewest number of coins needed to meet the total.
+             Returns 0 if the total is 0 or less.
+             Returns -1 if the total cannot be met by any combination of
+             the given coins.
+    """
     if total <= 0:
         return 0
 
